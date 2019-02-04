9c989581dccac225c5166ba9bad3d6302090f56b
megha13 <megha.sinha13@gmail.com>
2019-02-04 20:37:31 +0100

    wrote new program using javascript array and object

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-alert('Hello world');
+var pokemon1 ={
+  name:"Bulbasaur",
+  height:7,
+  type:"grass"
+};
+var pokemon2 ={
+  name:"Ivysaur",
+  height:6,
+  type:"poison"
+};
+var pokemon3 ={
+  name:"Venusaur",
+  height:8,
+  type:"grass"
+};
+
+var repository=[pokemon1,pokemon2,pokemon3];
+var maxHeight=7;
+
+for( var i = 0 ; i < repository.length ; i++ )
+{
+  if( repository[i].height > maxHeight)
+  {
+    document.write( repository[i].name + "\n" + "(" + "height:" + "\n" + repository[i].height + ")-Wow that's too big!" );
+    document.write( "<br>" );
+  }
+  else
+  {
+    document.write( repository[i].name + "\n" + "(" + "height:" + "\n" + repository[i].height + ")" );
+    document.write( "<br>" );
+  }
+}
