a799ce364f54c14fd73ed70cbb3ed08ea1e9b604
megha13 <megha.sinha13@gmail.com>
2019-02-08 23:15:29 +0100

    task5:DOM related task  with event handler

--- js/script.js
+++ js/script.js
@@ -1,30 +1,40 @@
 var pokemonRepository=(function () {
-  var pokemon1 ={
-    name:"Bulbasaur",
-    height:7,
-    type:"grass"
-  };
-  var pokemon2 ={
-    name:"Ivysaur",
-    height:6,
-    type:"poison"
-  };
-  var pokemon3 ={
-    name:"Venusaur",
-    height:8,
-    type:"grass"
-  };
+  var repository=[];
 
-  var repository=[pokemon1,pokemon2,pokemon3];
-  var count_property = 0;
+  /* showing details properties of pokemon object*/
+  function showDetails(new_element)
+  {
+    console.log( new_element.name ,  new_element.height , new_element.type );
+  }
 
+  /* This function  creats new element and adds into the DOM */
+  function addListItem(new_element)
+  {
+    var $element=document.createElement('li');
+    $element.classList.add('myList');
+    var $button=document.createElement('button');
+    $button.setAttribute('type', 'button');
+    /*To add Textual content to DOM element*/
+    $button.innerText = new_element.name;
+    $element.appendChild($button);
+    /* Now, steps for putting the new element in the DOM */
+    var $ul = document.querySelector('.pokedox');
+    $ul.appendChild($element);
+    var $button1 = document.querySelector('button');
+    /*adding click event to display properties of pokemon*/
+    $element.addEventListener('click', function(event)
+    {
+     showDetails(new_element);
+    });
+  }
 
-  function add(repositorys)
+  /*Function verify candidate Pokemon and Adds verified Pokemons to the repository*/
+  function add(new_pokemon)
   {
     var fill_object = 0;
-    if(( typeof(repositorys) ==='object' ) )
+    if(( typeof(new_pokemon) ==='object' ) )
     {
-      Object.keys(repositorys).forEach(function(property) {
+      Object.keys(new_pokemon).forEach(function(property) {
       fill_object = 0;
       if( (property == 'name') || (property == 'height') || (property == 'type') )
       {
@@ -34,39 +44,56 @@ var pokemonRepository=(function () {
     }
     if(fill_object == 1)
     {
-      repository.push(repositorys);
+      repository.push(new_pokemon);
     }
   }
+
+  /*Function retrievs the complete repository*/
   function getAll()
   {
     return repository;
   }
-  return{
+
+  return  {
     add:add,
-    getAll:getAll
+    addListItem:addListItem,
+    getAll:getAll,
+    showDetails:showDetails
   };
 })();
 
-
-var pokemon4 ={
-  name:"Charmander",
+/*Declaring candidate Pokemons*/
+var pokemon1 ={
+  name:"Bulbasaur",
+  height:7,
+  type:"grass"
+};
+var pokemon2 ={
+  name:"Ivysaur",
+  height:6,
+  type:"poison"
+};
+var pokemon3 ={
+  name:"Venusaur",
   height:8,
   type:"grass"
 };
 
-pokemonRepository.add(pokemon4);
+var pokemon4 ={
+  name:"Chikusaur",
+  height:4,
+  type:"grass",
+  age: 10
+};
 
-pokemonRepository.getAll().forEach(function(element)
+/*Adding candidate Pokemon to the repository*/
+pokemonRepository.add(pokemon1);
+pokemonRepository.add(pokemon2);
+pokemonRepository.add(pokemon3);
+pokemonRepository.add(pokemon4);/*not a valid pokemon hence must not be added in the repository*/
+
+/*DOM Operation: Call addlistItem which manipulates the DOM and insert candidate pokemons */
+pokemonRepository.getAll().forEach(function(new_element)
 {
-  if( element.height > 7)
-  {
-    document.write( element.name + "\n" + "(" + "height:" + "\n" + element.height + ")-Wow that's too big!" );
-    document.write( "<br>" );
-  }
-  else
-  {
-    document.write( element.name + "\n" + "(" + "height:" + "\n" + element.height + ")" );
-    document.write( "<br>" );
-  }
-
+  pokemonRepository.addListItem(new_element);
 });
