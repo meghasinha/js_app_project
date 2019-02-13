d3d070b31a01a5a67a10209e73e6a09e8c59ad4d
megha13 <megha.sinha13@gmail.com>
2019-02-13 09:28:29 +0100

    Bonus task 6: added and removed wait messages

--- js/script.js
+++ js/script.js
@@ -5,9 +5,48 @@ var pokemonRepository=(function () {
   /* showing details properties of pokemon object*/
   function showDetails(item)
   {
+    pokemonRepository.showPleaseWait();
     pokemonRepository.loadDetails(item).then(function () {
+    pokemonRepository.hidePleaseWait();
     console.log(item);   });
   }
+
+
+  function showPleaseWait() {
+    var $newElement = document.createElement('div');
+    $newElement.innerText = 'Please wait..';
+    $newElement.setAttribute('id', 'new-id');
+    $newElement.getAttribute('id');
+     $newElement.classList.add('new-class');
+    var $h1 = document.querySelector('h1');
+/*
+    var $modalLoading = '<div class="modal" id="pleaseWaitDialog" data-backdrop="static" data-keyboard="false role="dialog">\
+           <div class="modal-dialog">\
+              <div class="modal-content">\
+                  <div class="modal-header">\
+                      <h4 class="modal-title">Please wait...</h4>\
+                  </div>\
+                  <div class="modal-body">\
+                      <div class="progress">\
+                        <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar"\
+                        aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:100%; height: 40px">\
+                        </div>\
+                      </div>\
+                  </div>\
+              </div>\
+          </div>\
+      </div>';
+    */
+      $h1.appendChild($newElement);
+  }
+
+  /**
+ * Hides "Please wait" overlay. See function showPleaseWait().
+ */
+function hidePleaseWait() {
+  var $elementToRemove = document.querySelector('.new-class');
+  $elementToRemove.parentElement.removeChild($elementToRemove);
+}
 
   /* This function  creats new element and adds into the DOM */
   function addListItem(new_element)
@@ -97,7 +136,9 @@ function loadDetails(item)
     getAll:getAll,
     showDetails:showDetails,
     loadList:loadList,
-    loadDetails:loadDetails
+    loadDetails:loadDetails,
+    showPleaseWait:showPleaseWait,
+    hidePleaseWait:hidePleaseWait
   };
 })();
 
