0fdaa8bd859375b6548043df035fa1cd4b1d6ec2
megha13 <megha.sinha13@gmail.com>
2019-02-16 19:47:11 +0100

    Task 7:Created a modal for pokedox detail information

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var pokemonRepository=(function () {
   var repository=[];
   var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  var $modalContainer = document.querySelector('#modal-container');
+  var $modalContainer1=document.querySelector('#modal-container1');
 
   /* showing details properties of pokemon object*/
   function showDetails(item)
@@ -8,45 +10,37 @@ var pokemonRepository=(function () {
     pokemonRepository.showPleaseWait();
     pokemonRepository.loadDetails(item).then(function () {
     pokemonRepository.hidePleaseWait();
-    console.log(item);   });
+    pokemonRepository.showModal(item);
+       });
   }
 
-
+  /*Created a new modal to display wait message */
   function showPleaseWait() {
-    var $newElement = document.createElement('div');
-    $newElement.innerText = 'Please wait..';
-    $newElement.setAttribute('id', 'new-id');
-    $newElement.getAttribute('id');
-     $newElement.classList.add('new-class');
-    var $h1 = document.querySelector('h1');
-/*
-    var $modalLoading = '<div class="modal" id="pleaseWaitDialog" data-backdrop="static" data-keyboard="false role="dialog">\
-           <div class="modal-dialog">\
-              <div class="modal-content">\
-                  <div class="modal-header">\
-                      <h4 class="modal-title">Please wait...</h4>\
-                  </div>\
-                  <div class="modal-body">\
-                      <div class="progress">\
-                        <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar"\
-                        aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:100%; height: 40px">\
-                        </div>\
-                      </div>\
-                  </div>\
-              </div>\
-          </div>\
-      </div>';
-    */
-      $h1.appendChild($newElement);
+  /*var $newElement = document.createElement('div');
+  $newElement.innerText = 'Please wait..';
+  $newElement.setAttribute('id', 'new-id');
+  $newElement.getAttribute('id');
+   $newElement.classList.add('new-class');
+  var $h1 = document.querySelector('h1');
+    $h1.appendChild($newElement);*/
+   $modalContainer1.innerHTML='';
+    var modal1=document.createElement('div');
+    modal1.classList.add('modal1');
+    var text=document.createElement('p');
+    text.innerHTML="please wait its loading";
+    modal1.appendChild(text);
+    $modalContainer1.appendChild(modal1);
+    $modalContainer1.classList.add('is-visible');
   }
 
-  /**
- * Hides "Please wait" overlay. See function showPleaseWait().
- */
-function hidePleaseWait() {
-  var $elementToRemove = document.querySelector('.new-class');
-  $elementToRemove.parentElement.removeChild($elementToRemove);
-}
+    /**
+   * Hides "Please wait" overlay. See function showPleaseWait().
+   */
+  function hidePleaseWait() {
+    /*var $elementToRemove = document.querySelector('.new-class');
+    $elementToRemove.parentElement.removeChild($elementToRemove);*/
+    $modalContainer1.classList.remove('is-visible');
+  }
 
   /* This function  creats new element and adds into the DOM */
   function addListItem(new_element)
@@ -62,11 +56,16 @@ function hidePleaseWait() {
     var $ul = document.querySelector('.pokedox');
     $ul.appendChild($element);
     var $button1 = document.querySelector('button');
-    /*adding click event to display properties of pokemon*/
-    $element.addEventListener('click', function(event)
+    /*adding pointer event to display properties of pokemon*/
+    $element.addEventListener('pointerdown', function(event)
     {
-     showDetails(new_element);
-    });
+      /*Currently showing details for touch and mouse click */
+      switch(event.pointerType) {
+        case "touch":
+        case "mouse":
+          showDetails(new_element);
+      }
+   });
   }
 
 
@@ -90,39 +89,95 @@ function hidePleaseWait() {
     }
   }
 
-/*Getting List of Pokemons from the specified URL */
-function loadList()
-{
-  return fetch(apiUrl).then(function (response) {
-    return response.json();
-  }).then(function (json) {
-    json.results.forEach(function (item) {
-    var pokemon = {
-    name: item.name,
-    detailsUrl: item.url
-    };
-    add(pokemon);
-    });
-  }).catch(function (e) {
-    console.error(e);
-  })
-}
+  /*Getting List of Pokemons from the specified URL */
+  function loadList()
+  {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+      var pokemon = {
+      name: item.name,
+      detailsUrl: item.url
+      };
+      add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
 
-/*Fetching the details of each element from the specific URL*/
-function loadDetails(item)
-{
-  var url = item.detailsUrl;
-  return fetch(url).then(function (response) {
-    return response.json();
-  }).then(function (details) {
-    // Now we add the details to the item
-    item.imageUrl = details.sprites.front_default;
-    item.height = details.height;
-    item.types = Object.keys(details.types);
-  }).catch(function (e) {
-    console.error(e);
+  /*Fetching the details of each element from the specific URL*/
+  function loadDetails(item)
+  {
+    var url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = Object.keys(details.types);
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+
+  function showModal(item) {
+    // Clear all existing modal content
+
+    $modalContainer.innerHTML = '';
+
+    var modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    // Add the new modal content
+    var closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('pointerdown', hideModal);
+
+    var titleName = document.createElement('p');
+    titleName.innerText = 'Name:'+ item.name;
+    var titleHeight = document.createElement('p');
+    titleHeight.innerText = 'Height:'+item.height;
+    var titleType=document.createElement('p');
+    titleType.innerText='Type:'+ item.types;
+    var imgElement=document.createElement('img');
+    imgElement.classList.add('responsive');
+    imgElement.setAttribute("src",item.imageUrl);
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleName);
+    modal.appendChild(titleHeight);
+    modal.appendChild(titleType);
+    modal.appendChild(imgElement);
+    $modalContainer.appendChild(modal);
+    $modalContainer.classList.add('is-visible');
+  }
+
+  /*To remove the modal display */
+  function hideModal() {
+    $modalContainer.classList.remove('is-visible');
+  }
+
+
+  /*  To remove the modal display  when Esc is pressed */
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && $modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
   });
-}
+
+  $modalContainer.addEventListener('pointerdown', (e) => {
+    // Since this is also triggered when clicking INSIDE the modal container,
+    // We only want to close if the user clicks directly on the overlay
+    var target = e.target;
+    if (target === $modalContainer) {
+      hideModal();
+    }
+  });
+
 
   /*Function retrievs the complete repository*/
   function getAll()
@@ -138,7 +193,10 @@ function loadDetails(item)
     loadList:loadList,
     loadDetails:loadDetails,
     showPleaseWait:showPleaseWait,
-    hidePleaseWait:hidePleaseWait
+    hidePleaseWait:hidePleaseWait,
+    showModal:showModal,
+    hideModal:hideModal
+
   };
 })();
 
